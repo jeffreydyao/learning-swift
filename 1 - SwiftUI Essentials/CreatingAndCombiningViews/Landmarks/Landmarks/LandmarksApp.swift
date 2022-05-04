d83c7e44f63bd34c84933a51e8d33c3883e9bcc8
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-04 16:05:14 +1000

    Completed Creating and Combining Views

---
+++ 1 - SwiftUI Essentials/CreatingAndCombiningViews/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeffrey Yao on 4/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
