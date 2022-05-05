600fd878de68640080e2fd87d60639867139b23d
Jeffrey Yao <33971845+jeffreydyao@users.noreply.github.com>
2022-05-05 22:21:14 +1000

    ✅ Composing Complex Interfaces

---
+++ 1 - SwiftUI Essentials/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Jeffrey Yao on 5/5/2022.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) {key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .listStyle(PlainListStyle())
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .previewDevice("iPhone 13 Pro")
+            .environmentObject(ModelData())
+    }
+}
